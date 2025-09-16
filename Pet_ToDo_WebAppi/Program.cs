47058d58b827e832eec8b38f6b0eb2e7bbfc3ef1
Vlad <vnkolevatov@gmail.com>
2025-09-16 21:42:32 +0500

    Https setup

--- Pet_ToDo_WebAppi/Program.cs
+++ Pet_ToDo_WebAppi/Program.cs
@@ -1,4 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args); 
  
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.MapGet("/", () => "Hello World!");
+
 app.Run();
+
+
