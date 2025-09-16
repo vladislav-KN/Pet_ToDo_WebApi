b378a978288122930f1f7adf299809a06b9f7770
Vlad <vnkolevatov@gmail.com>
2025-09-16 21:48:32 +0500

    Swagger setuped

--- Pet_ToDo_WebAppi/Program.cs
+++ Pet_ToDo_WebAppi/Program.cs
@@ -1,8 +1,18 @@
-var builder = WebApplication.CreateBuilder(args); 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
  
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
-app.MapGet("/", () => "Hello World!");
+  
 
 app.Run();
 
